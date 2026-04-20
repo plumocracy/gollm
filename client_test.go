039dd13e92d7cf15055fd534dc164aaaf9f596ad
Plum Hill <evelynplumhill@gmail.com>
2026-04-20 05:51:41 -0400

    Changed test to fail to ensure that failing tests actually fail

--- client_test.go
+++ client_test.go
@@ -5,7 +5,7 @@ import "testing"
 // This is just a base test to ensure that things are running properly in github actions.
 func TestOneNeqZero(t *testing.T) {
 	value := 1
-	if value == value-1 {
+	if value != value-1 {
 		t.Errorf("Value %d == %d???", value, value-1)
 	}
 }
